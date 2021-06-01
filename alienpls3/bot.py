f3837a8f6110e859877b3722c3868b4894d0b539
Lachlan <12870244+lachlantula@users.noreply.github.com>
2021-06-01 16:53:02 +1000

    Add files via upload

---
+++ alienpls3/bot.py
@@ -0,0 +1,26 @@
+import os
+import discord
+
+TOKEN = 'ODQ4NzUwMTUxOTAzNDc3Nzkw.YLRKLQ.ftWTjPJoz2Ci4id0P6BSG4f-Wb4'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  messageContent = message.content.lower()
+  
+  if ('alienpls3' in messageContent):
+    response = 'https://media.discordapp.net/attachments/758617780387315755/838040094010769418/alienpls3.gif'
+    await message.channel.send(response)
+  elif ('get in there' in messageContent or 'lewis' in messageContent):
+    response = 'https://media.discordapp.net/attachments/758617780387315755/848889035229364224/get-in-there-lewis.gif'
+    await message.channel.send(response)
+
+client.run(TOKEN)
